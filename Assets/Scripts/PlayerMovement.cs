7bbb36b8ef48059b0057d790702ba3d1af485653
IlyaPlekhanov <i.plekhanov@greengreystudio.com>
2023-05-09 15:55:10 +0300

    player sprites + player movement + animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Animator))]
+    public class PlayerMovement: MonoBehaviour
+    {
+        [SerializeField] private CharacterController2D m_characterController2D;
+        [SerializeField] private float m_runSpeed;
+
+        private Animator m_animator;
+        private Rigidbody2D m_rigidbody;
+        private float m_horizontalMove;
+        private bool m_jump = false;
+        private bool m_croach = false;
+        private bool m_inAir = false;
+
+        private void Awake()
+        {
+            m_rigidbody = GetComponent<Rigidbody2D>();
+            m_animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            m_horizontalMove = Input.GetAxisRaw("Horizontal") * m_runSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                m_animator.SetTrigger("Jump");
+                m_jump = true;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.LeftAlt))
+                m_croach = true;
+        }
+
+        public void SetInAirStatus()
+        {
+            m_inAir = true;
+        }
+
+        private void FixedUpdate()
+        {
+            m_characterController2D.Move(m_horizontalMove * Time.fixedDeltaTime, m_croach, m_jump);
+            m_animator.SetFloat("Speed", m_horizontalMove*m_horizontalMove);
+            if (m_characterController2D.m_onGround && m_inAir)
+            { 
+                m_animator.SetTrigger("Landing");
+                m_inAir = false;
+            }
+               
+            m_jump = false;
+        }
+    }
+}
